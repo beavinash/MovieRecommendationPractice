3a7155539295315f0fa0d87406937037452bfcf4
beavinash <beavinash15@gmail.com>
2018-09-11 16:58:04 -0700

    final commit

---
+++ MovieRecommendation.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 16:04:58 2018
+
+@author: avi
+"""
+
+import pandas as pd
+
+# Reading user file
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv("u.user", sep='|', names=u_cols, encoding='latin-1')
+
+# Reading ratings file
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv("u.data", sep='\t', names=r_cols, encoding='latin-1')
+
+# Reading items file
+i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+ 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+ 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+items = pd.read_csv("u.item", sep='|', names='i_cols', encoding='latin-1')
+
+print(users.shape)
+print(users.head())
+print(ratings.shape)
+print(ratings.head())
+print(items.shape)
+print(items.head())
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings_base = pd.read_csv('ua.base', sep='\t', names='r_cols', encoding='latin-1')
+ratings_test = pd.read_csv('ua.test', sep='\t', names='r_cols', encoding='latin-1')
+
+print(ratings_base.shape)
+print(ratings_test.shape)
+
+import graphlab
+
+train_data = graphlab.SFrame(ratings_base)
+test_data = graphlab.SFrame(ratings_test)
+
+popularity_model = graphlab.popularity_recommender.create(train_data, user_id='user_id', item_id='item_id', target='rating')
+
+popularity_recomm = popularity_model.recommend(users=range(1,6), k = 5)
+popularity_recomm.print_rows(num_rows=25)
+
+ratings_base.groupby(by='movie_id')['rating'].mean().sort_values(ascending=False).head(20)
+
+#Train Model
+item_sim_model = graphlab.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating', similarity_type='pearson')
+#Make Recommendations:
+item_sim_recomm = item_sim_model.recommend(users=range(1,6),k=5)
+item_sim_recomm.print_rows(num_rows=25)
+
+model_performance = graphlab.compare(test_data, [popularity_model, item_sim_model])
+graphlab.show_comparison(model_performance,[popularity_model, item_sim_model])
+
